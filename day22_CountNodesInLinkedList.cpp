33818606777c9a6a564112d5950d9b5e0ba63fff
Kushagra Jain <jainkushagrawork@gmail.com>
2026-08-03 16:39:36 +0530

    Day 22

---
+++ day22_CountNodesInLinkedList.cpp
@@ -0,0 +1,80 @@
+/*
+Problem: Count Nodes in Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+Node * createNode(int data){
+    Node * newNode = new Node;
+    newNode->data = data;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+Node * addAtEnd(Node * &head, Node * &tail, int data){
+    Node * newNode = createNode(data);
+    if(head==nullptr){
+        head = tail = newNode;
+        return head;
+    }
+    tail->next = newNode;
+    tail = newNode;
+    return head;
+}
+
+int countNodes(Node *head){
+    int counter = 0;
+    Node *temp = head;
+    while(temp){
+        counter++;
+        temp = temp->next;
+    }
+    return counter;
+}
+
+void printLL(Node * head){
+    Node *temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Node * head = nullptr;
+    Node * tail = nullptr;
+    int elements;
+    cout<<"Number of elements in linked list? ";
+    cin>>elements;
+    cout<<"Start entering the elements of array"<<endl;
+    for(int i = 0; i < elements; i++){
+        int data;
+        cin>>data;
+        head = addAtEnd(head, tail, data);
+    }
+    printLL(head);
+    int count = countNodes(head);
+    cout<<"\n"<<count;
+    return 0;
+}
