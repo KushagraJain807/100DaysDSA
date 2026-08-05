5f2e6f164e2b7568f595f92106cbf19b188f5534
Kushagra Jain <jainkushagrawork@gmail.com>
2026-08-06 00:11:14 +0530

    Day 30

--- day30_PolynomialUsingLinkedList.cpp
+++ day30_PolynomialUsingLinkedList.cpp
@@ -23,3 +23,64 @@ Output:
 Explanation:
 Each node stores coefficient and exponent. Traverse nodes to print polynomial in decreasing exponent order.
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Poly{
+    float coeff;
+    int power;
+    Poly *next;
+};
+
+Poly * createTerm(float coeff, int pow){
+    Poly * newTerm = new Poly;
+    newTerm->coeff = coeff;
+    newTerm->power = pow;
+    newTerm->next = nullptr;
+    return newTerm;
+}
+
+void addTerm(Poly * &head, Poly * &tail, float coeff, int pow){
+    Poly * newTerm = createTerm(coeff, pow);
+    if(head==nullptr){
+        head = tail = newTerm;
+        return;
+    }
+    tail->next = newTerm;
+    tail = newTerm;
+}
+
+void printPoly(Poly *head){
+    Poly *temp = head;
+    while(temp){
+        cout << temp->coeff;
+        if(temp->power > 1){
+            cout << "x^" << temp->power;
+        }
+        else if(temp->power == 1){
+            cout << "x";
+        }
+        if(temp->next){
+            cout << " + ";
+        }
+        temp = temp->next;
+    }
+}
+
+int main(){
+    Poly *head = nullptr;
+    Poly *tail = nullptr;
+    cout<<"Enter number of terms you want to add in your polynomial: ";
+    int terms;
+    cin>>terms;
+    for(int i = 0; i < terms; i++){
+        float coeff;
+        int pow;
+        cin>>coeff;
+        cin>>pow;
+        addTerm(head, tail, coeff, pow);
+    }
+    printPoly(head);
+    return 0;
+}
